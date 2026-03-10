56ad810db23897553c5275da4a28228ff8b2eec3
epfessenheim <epfessenheim@gmail.com>
2026-03-10 14:58:05 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,7 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/digipad-ecole/service-worker.js");
-  });
-}
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode><App /></StrictMode>
+)
